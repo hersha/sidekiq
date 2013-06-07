8182d7e3efc86db37a08fbf2c221dda856903e51
Patrick Neff <pneff@anpi.com>
2013-06-07 10:28:17 -0500

    stuff

--- run.rb
+++ run.rb
@@ -1,7 +1,9 @@
 require './worker'
 
-File.open("lasgsx01.20120108183507.1026188.2158213975", "r") do |file|
-  file.each do |line|
-    HardWorker.perform_async(line, 5)
+10.times do
+  File.open("lasgsx01.20120108183507.1026188.2158213975", "r") do |file|
+    file.each do |line|
+      HardWorker.perform_async(line, 5)
+    end
   end
 end
